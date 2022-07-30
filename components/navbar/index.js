0b890b49e0a357d3fe49e441023b5feb2382574f
Júnior Matos <sk8.merp@gmail.com>
2022-07-29 22:00:50 -0300

    feat: add navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,7 +4,7 @@ import style from './index.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import Logo from '../../img/logo.svg';
+import Logo from '../../img/Logo.png';
 
 const navbar = () => {
   const router = useRouter()
@@ -24,8 +24,8 @@ const navbar = () => {
         <Image
         src={Logo}
         alt="Logo coderfaster"
-        width={50}
-        height={50}
+        width={150}
+        height={100}
         />
         <div onClick={()=>{setMenu(!menu)}} className={menu ? `${style.ContainerbuttonMenu + " "  + style.ativo}` : `${style.ContainerbuttonMenu}`}>
           <button></button>
@@ -34,29 +34,20 @@ const navbar = () => {
         </div>
         <nav className={menu ? `${style.menuMobile + " "  + style.ativo}` : `${style.menuMobile}`}>
           <ul>
-            <li><Link href="/"><a>HOME</a></Link></li>
-            <li><Link href="/sobre"><a>SOBRE</a></Link></li>
-            <li><Link href="/produtos"><a>PRODUTOS</a></Link></li>
-            <li><Link href="/exemplos"><a>EXEMPLOS</a></Link></li>
-            <li><Link href="/contato"><a>CONTATO</a></Link></li>
-            <li><Link href="/login"><a>LOGIN</a></Link></li>
+            <li><Link href="/"><a>INICIO</a></Link></li>
+            <li><Link href="/artigos"><a>ARTIGOS</a></Link></li>
+            <li><Link href="/creditos"><a>CREDITOS</a></Link></li>
+            <li><Link href="/ajude"><a>AJUDE-NOS</a></Link></li>
           </ul>
         </nav>
         <nav className={style.menu}>
           <ul>
-            <li><Link href="/"><a>HOME</a></Link></li>
-            <li><Link href="/sobre"><a>SOBRE</a></Link></li>
-            <li><Link href="/produtos"><a>PRODUTOS</a></Link></li>
-            <li><Link href="/exemplos"><a>EXEMPLOS</a></Link></li>
-            <li><Link href="/contato"><a>CONTATO</a></Link></li>
+            <li style={{color:"#FF010A"}}><Link href="/"><a>INICIO</a></Link></li>
+            <li><Link href="/artigos"><a>ARTIGOS</a></Link></li>
+            <li><Link href="/creditos"><a>CREDITOS</a></Link></li>
+            <li><Link href="/ajude"><a>AJUDE-NOS</a></Link></li>
           </ul>
         </nav>
-        <div className={style.login}>
-          <p>Já e cliente?</p>
-          <Link href="/login">
-            <button>LOGIN</button>
-          </Link>
-        </div>
     </header>
   )
 }
