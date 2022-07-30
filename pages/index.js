c9528f8c7900b2e8bf1aa5c5030cc5c232a49f77
Júnior Matos <sk8.merp@gmail.com>
2022-07-29 22:22:12 -0300

    feat: new feat

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <section>
-<<<<<<< HEAD
       <Navbar/>
       <div className={styles.containerBemvindo}>
         <h1>BEM VINDO AVENTUREIRO</h1>
         <p>ESTE SITE É UM GUIA PARA TODOS OS JOGADORES DA MESA DE D&D DO JÚNIOR MATOS, AQUI O SISTEMA SE BASEIA NO D&D COM ALGUMAS COISINHAS A MENOS E A MAIS, FIQUE A VONTADE PARA EXPLORAR O SITE E ENCONTRAR O QUE VOÇE PROCURA,CLIQUE AQUI PARA VOCÊ ENCONTRA TODOS OS ARTIGOS NECESSÁRIOS.</p>
       </div>
-=======
-      
->>>>>>> 7ffae56ea2addc1017e198b1250da64f80706a8c
     </section>
   )
 }
